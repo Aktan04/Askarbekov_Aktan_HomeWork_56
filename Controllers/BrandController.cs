ba75e582ce0a9a56343592b83f28726e036dc512
Askarbekov Aktan <akn392204@gmail.com>
2024-04-29 15:47:11 +0600

    Add BrandController and default Views for this

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,81 @@
+using AuthProduct.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthProduct.Controllers;
+
+public class BrandController : Controller
+{
+    private readonly MobileContext _db;
+
+    public BrandController(MobileContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var brands = _db.Brands.ToList();
+        return View(brands);
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Brand brand)
+    {
+        if (_db.Brands.Any(c => c.Name == brand.Name))
+        {
+            ModelState.AddModelError("Name", "Бренд с таким названием уже существует!");
+            return View(brand);
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Brands.Add(brand);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(brand);
+    }
+    
+    public IActionResult Edit(int id)
+    {
+        var brand = _db.Brands.Find(id);
+        if (brand == null)
+        {
+            return NotFound();
+        }
+        return View(brand);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Brand brand, int id)
+    {
+        if (_db.Brands.Any(c => c.Name == brand.Name && c.Id != id))
+        {
+            ModelState.AddModelError("Name", "Категория с таким названием уже существует!");
+            return View(brand);
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Update(brand);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Brand");
+        }
+        return View(brand);
+    }
+    
+    public IActionResult Delete(int id)
+    {
+        var brand = _db.Brands.FirstOrDefault(b => b.Id == id);
+        if (brand == null)
+        {
+            return NotFound();
+        }
+        _db.Brands.Remove(brand);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
