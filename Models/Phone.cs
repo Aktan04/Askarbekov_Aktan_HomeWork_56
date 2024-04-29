b76d79667bbad96e1cab236b5e969126c8b3abba
Askarbekov Aktan <akn392204@gmail.com>
2024-04-29 15:43:34 +0600

    Add model of Phone and PhoneReview

---
+++ Models/Phone.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AuthProduct.Models;
+
+public class Phone
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Поле 'Наименование' обязательно для заполнения")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "Поле 'Компания' обязательно для заполнения")]
+    public string Company { get; set; }
+    [Range(100, 2000)]
+    [Required]
+    public int Price { get; set; }
+    [BindNever]
+    public ICollection<PhoneReview>? Reviews { get; set; }
+    [Required(ErrorMessage = "Пожалуйста, выберите бренд")]
+    public int? BrandId { get; set; }
+    public Brand? Brand { get; set; }
+}
---
+++ Models/PhoneReview.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthProduct.Models;
+
+public class PhoneReview
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Пожалуйста, укажите ваше имя")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Пожалуйста, оцените телефон от 0 до 5")]
+    [Range(0, 5, ErrorMessage = "Оценка должна быть от 0 до 5")]
+    public int Rating { get; set; }
+
+    public string ReviewText { get; set; }
+
+    [ForeignKey("Phone")]
+    public int PhoneId { get; set; }
+    public Phone? Phone { get; set; }
+}
