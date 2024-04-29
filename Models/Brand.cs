538f84d31ccd8ffc6ead65126742fa771c20b995
Askarbekov Aktan <akn392204@gmail.com>
2024-04-29 15:42:34 +0600

    Add model of Brand.cs

---
+++ Models/Brand.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthProduct.Models;
+
+public class Brand
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Поле 'Наименование' обязательно для заполнения")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Поле 'Email' обязательно для заполнения")]
+    [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Поле 'Дата основания' обязательно для заполнения")]
+    [Remote(action: "CheckDate", controller: "Validation", ErrorMessage = "Дату из будущего или ранее чем 100 лет. ")]
+    public DateTime FoundationDate { get; set; }
+}
